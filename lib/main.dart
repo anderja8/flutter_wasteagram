2950db11e6ae14bda1a771dcdb0578f16511c7a4
Jacob Anderson <anderja8@oregonstate.edu>
2020-08-08 08:50:32 -0700

    Created boilerplate and firebase P.O.C. main page

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Does this even do anything?',
+      theme: ThemeData.dark(),
+      home: POCHome(),
+    );
+  }
+}
+
+class POCHome extends StatefulWidget {
+  @override
+  _POCHomeState createState() => _POCHomeState();
+}
+
+class _POCHomeState extends State<POCHome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('Flutter POC')),
+        body: StreamBuilder(
+            stream: Firestore.instance.collection('wastePosts').snapshots(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return CircularProgressIndicator();
+              return SingleChildScrollView(
+                  child: Column(
+                children: [
+                  Text(
+                      'There are ${snapshot.data.documents.length} files in the firebase db.')
+                ],
+              ));
+            }));
+  }
+}
